3bdfcbebd80ac503719c2729b7bf3fbbdfa2e94b
Love Sharma <love.sharma@indexexchange.com>
2020-11-10 10:44:30 -0500

    added required properties to valid config

--- test/spec/modules/ixBidAdapter_spec.js
+++ test/spec/modules/ixBidAdapter_spec.js
@@ -126,7 +126,9 @@ describe('IndexexchangeAdapter', function () {
             'video/mp4',
             'video/webm'
           ],
-          minduration: 0
+          minduration: 0,
+          maxduration: 60,
+          protocols: [2]
         },
         size: [400, 100]
       },
